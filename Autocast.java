c070c8907e760097ab5e38bb62e53e66ce0fda1c
hariharan-075 <2420080075@klh.edu.in>
2025-08-02 09:10:32 +0530

    Add files via upload

---
+++ Autocast.java
@@ -0,0 +1,13 @@
+package MyProject;
+
+public class Autocast {
+	public static void main(String args[]) {
+		int a=5;
+		double b=a;
+		float c=a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
---
+++ manualcast.java
@@ -0,0 +1,10 @@
+package MyProject;
+
+public class manualcast {
+	public static void main(String args[]) {
+		double a=6.5;
+		int b= (int)a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
